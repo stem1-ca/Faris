00e4b986cd2aada1c123da770d5bc6c54bf68784
Faheem Khan <faheem5948@gmail.com>
2019-11-02 11:55:35 -0400

    Add files via upload

--- Discord-Bot/Rock-Paper-Scissors/index.js
+++ Discord-Bot/Rock-Paper-Scissors/index.js
@@ -1,14 +1,22 @@
+// Made by Faris & Fluffy
+// This is a Rock, paper, Scissors discord bot made on November 2nd 2019!
+// This code took two weeks to make.
+
+// Importing necessary libaries
+
 const discord = require("discord.js")
 const client = new discord.Client()
+
+// Discord token
 const token = "Add-Token-Here"
 
 client.on("ready", () => {
     console.log("login")
 })
-
+// When player chooses rock and bot chooses scissor player wins
 client.on("message", (message) => {
     if (message.content === "!help") {
-        message.reply("Type `!Start!` and then type in either Rock, paper, or Scissors and we can start playing!")
+        message.reply("Type To play the game, type !start rock, !start paper, or start scissor! The bot will pick either rock, paper, or scissors. The bot will pick randomly so it is pretty hard to win. Anyways, enjoy!")
     } else if (message.content.startsWith("!start")) {
         var randomnumber = Math.floor(Math.random() * 3)
         console.log(randomnumber)
