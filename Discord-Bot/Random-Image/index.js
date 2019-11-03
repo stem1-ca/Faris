3b45b899bb0b0a7a075d2ac66469f71ba6c00338
Faheem Khan <faheem5948@gmail.com>
2019-11-02 21:30:42 -0400

    Update index.js

--- Discord-Bot/Random-Image/index.js
+++ Discord-Bot/Random-Image/index.js
@@ -1,9 +1,6 @@
 // Made by Faris & Fluffy
-// This is a Rock, paper,  Scissors discord bot made on November 2nd 2019!
-// This code took two weeks to make.
 
 // Importing necessary libaries
-
 const discord = require("discord.js")
 const client = new discord.Client()
 const fetch = require("node-fetch")
